497591473ae5a2927c52a70f1eda10adb85b70c6
ECHARHBILI-Elhoussaine <e.echarhbili@gmail.com>
2025-01-19 00:15:30 +0100

    FirstCommit
    FIRSTCOMMIT

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import ToDoList from './ToDoList.jsx'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+
+  return(<ToDoList/>)
+}
+ 
+
